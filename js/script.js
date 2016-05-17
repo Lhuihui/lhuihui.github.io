0da7fe718c9817604b54972cb7ce16dfb4e438b3
lhuihui <lxhhuihui@gmail.com>
2016-05-17 05:11:30 -0500

    add squares

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
+$(document).ready(function() {
 
+  function scrollTo($element) {
+    $('html, body').animate({ scrollTop: $($element).height() }, 1000);
+  }
+
+  function squarify(DOMElement) {
+    var width = $(DOMElement)[0].getBoundingClientRect().width;
+    $(DOMElement).css('height', width + 'px');
+  }
+
+  function updateSquares() {
+    $('.js-square').each(function() {
+      squarify(this);
+    });
+  }
+
+  $(window).resize(function() {
+    updateSquares();
+  });
+
+  $(document).on('click', '.js-menu', function() {
+    $(this).toggleClass('js-menu-show');
+  });
+
+  $(document).on('click', '.js-resume', function(event) {
+    event.stopPropagation();
+    scrollTo($('#resume'));
+  })
+
+  updateSquares();
+});
