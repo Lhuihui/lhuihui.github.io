c169736f2946f5b3934f05df0b24f86bba1b1b3e
XIAOHUI LUO <evaluo_xhl@us.ibm.com>
2016-05-17 20:17:08 -0500

    style text tile

--- js/script.js
+++ js/script.js
@@ -9,14 +9,26 @@ $(document).ready(function() {
     $(DOMElement).css('height', width + 'px');
   }
 
+  function rectanglify(DOMElement) {
+    var width = $(DOMElement)[0].getBoundingClientRect().width;
+    $(DOMElement).css('height', width / 2 + 'px');
+  }
+
   function updateSquares() {
     $('.js-square').each(function() {
       squarify(this);
     });
   }
 
+  function updateRectangles() {
+    $('.js-rectangle').each(function() {
+      rectanglify(this);
+    });
+  }
+
   $(window).resize(function() {
     updateSquares();
+    updateRectangles();
   });
 
   $(document).on('click', '.js-menu', function() {
