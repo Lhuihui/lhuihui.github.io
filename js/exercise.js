e819207f10d2b833f84a9a85502d9df60c020faa
XIAOHUI LUO <evaluo_xhl@us.ibm.com>
2016-10-01 22:14:58 -0500

    add animation

---
+++ js/exercise.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  window.sr = ScrollReveal();
+  sr.reveal('.exercise--content-1');
+  sr.reveal('.exercise--content-2');
+  sr.reveal('.exercise--content-3');
+  sr.reveal('.exercise--content-4');
+  sr.reveal('.exercise--content-5');
+  sr.reveal('.exercise--content-6');
+  sr.reveal('.exercise--content-7');
+  sr.reveal('.exercise--content-8');
+  sr.reveal('.exercise--content-9');
+  sr.reveal('.exercise--content-10');
+  sr.reveal('.exercise--content-11');
+  sr.reveal('.exercise--content-12');
+  sr.reveal('.exercise--content-13');
+  sr.reveal('.exercise--content-14');
+  sr.reveal('.exercise--content-15');
+});
